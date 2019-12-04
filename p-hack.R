a1bba209747c8ff153fd7392f215d544a34c978c
Glenn Patrick Williams <33417082+gpwilliams@users.noreply.github.com>
2019-12-04 08:09:42 +0000

    Create p-hack.R
    Added the basic functions for simulating optional stopping with and without p-value correction for a simple two group design relying on a t-test.

---
+++ p-hack.R
@@ -0,0 +1,69 @@
+# simulate data
+library(tidyverse)
+
+# sample data and make a tibble with DV and ID based on input group sample size
+# this is fixed at means of 100 and sd of 15 for each group
+sample_groups <- function(n_group){
+  group_one <- rnorm(n = n_group, mean = 100, sd = 15)
+  group_two <- rnorm(n = n_group, mean = 100, sd = 15)
+  
+  data <- tibble(
+    dv = c(group_one, group_two),
+    id = c(rep("group_one", n_group),
+           rep("group_two", n_group)
+           )
+  )
+}
+
+# run a t-test and extract the p-value for a tibble
+# columns must be named dv and id for dependent variables and group ID
+test_groups <- function(tibble_data) {
+  test <- t.test(dv ~ id, data = tibble_data, paired = FALSE)
+  test$p.value
+}
+
+# optional stopping without correction:
+# sample data and extract p-values from a t-test
+# if non-significant, sample additional people and retest
+# sample up to a certain number of additional blocks of subjects
+# using loop_total
+p_hack <- function(additions = 1, original_n = 30, additional_n = 10){
+  run <- 0
+  original_data <- sample_groups(original_n)
+  p_val <- test_groups(original_data)
+  
+  while (p_val > .05 & run < additions) {
+    new_data <- rbind(original_data, sample_groups(additional_n))
+    p_val <- test_groups(new_data)
+    run <- run + 1
+  }
+  p_val
+}
+
+# sample the data at various numbers of additional tests
+# then run p-hacked tests
+hacked_p <- tibble(
+  additional_tests = 0:30, 
+  p_vals = numeric(31)
+)
+n <- 1000
+
+for(i in seq_along(hacked_p$additional_tests)) {
+  ps <- replicate(n = n, p_hack(hacked_p$additional_tests[i]))
+  hacked_p$p_vals[i] <- mean(ps < .05) 
+}
+
+p_hack_rates <- ggplot(hacked_p, aes(x = additional_tests, y = p_vals)) +
+  geom_line() +
+  geom_point() +
+  scale_x_continuous(breaks = seq(0, 30, 5)) +
+  scale_y_continuous(breaks = seq(0, 0.5, 0.05), labels = scales::percent) +
+  labs(
+    x = "Number of Additional Blocks of 10 Participants Tested",
+    y = "Percentage of False Positives",
+    title = "False Positive Rates from Uncorrected Sequential Testing.",
+    subtitle = paste(n, "Iterations Per Test.")
+  ) +
+  theme_bw()
+
+ggsave("p_hack_rates.png", p_hack_rates, height = 8, width = 12)
